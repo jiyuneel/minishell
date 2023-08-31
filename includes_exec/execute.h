0ae8ed80cc9c9fd20c06db9af1dada8d9aa83290
Jihyun Kim <ksk208105@gmail.com>
2023-08-31 23:58:19 +0900

    Add: [include_exec folder] 실행부 헤더 추가

---
+++ includes_exec/execute.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   execute.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jihykim2 <jihykim2@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/30 19:45:22 by jihykim2          #+#    #+#             */
+/*   Updated: 2023/08/31 23:56:47 by jihykim2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EXECUTE_H
+# define EXECUTE_H
+
+# include "../includes/minishell.h"
+# include "./include.h"
+# include "./define.h"
+
+/* [ function prototype ] */
+
+#endif
---
+++ includes_exec/include.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   include.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jihykim2 <jihykim2@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/30 19:45:22 by jihykim2          #+#    #+#             */
+/*   Updated: 2023/08/31 23:56:16 by jihykim2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HEADER_H
+# define HEADER_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <signal.h>
+# include <dirent.h>
+# include <term.h>
+# include <sys/ioctl.h>
+# include <sys/wait.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
+#endif

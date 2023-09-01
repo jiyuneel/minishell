be769d395e6c0c587cd7cb18f735fb189474259f
이지윤 <02leejiyun@naver.com>
2023-09-01 19:25:56 +0900

    Add: [parsing.h] parsing 헤더 파일 추가

---
+++ parsing/parsing.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parsing.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jiyunlee <jiyunlee@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/01 18:08:08 by jiyunlee          #+#    #+#             */
+/*   Updated: 2023/09/01 18:18:09 by jiyunlee         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PARSING_H
+# define PARSING_H
+
+# include "../includes/minishell.h"
+
+void	env_init(t_env_info **env_info, char **envp);
+
+#endif

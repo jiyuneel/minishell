1e76031f7af3ec79ad22aab104d7141de4393549
이지윤 <02leejiyun@naver.com>
2023-09-06 20:14:10 +0900

    Modify: [minishell.c] readline으로 입력받기

--- parsing/minishell.c
+++ parsing/minishell.c
@@ -6,16 +6,17 @@
 /*   By: jiyunlee <jiyunlee@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/30 19:51:59 by jiyunlee          #+#    #+#             */
-/*   Updated: 2023/09/01 18:18:29 by jiyunlee         ###   ########.fr       */
+/*   Updated: 2023/09/06 20:11:33 by jiyunlee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parsing.h"
 
-int main(int argc, char **argv, char **envp)
+int	main(int argc, char **argv, char **envp)
 {
 	// t_shell_info	shell_info;
 	t_env_info		*env_info;
+	char			*str;
 
 	(void) argv;
 	if (argc != 1)
@@ -23,5 +24,15 @@ int main(int argc, char **argv, char **envp)
 	env_init(&env_info, envp);
 	for (t_env_info *tmp = env_info; tmp; tmp = tmp->next)
 		ft_printf("key: %s, value: %s\n", tmp->key, tmp->value);
-	return 0;
+	while (1)
+	{
+		str = readline("jijishell$ ");
+		if (!str)		// ctrl+D
+			break ;
+		// cmd_init()
+		// ft_printf("str = %s\n", str);
+		add_history(str);
+		free(str);
+	}
+	return (0);
 }

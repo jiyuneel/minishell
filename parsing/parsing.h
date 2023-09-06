df9fc61fb1201a4fdeb4f9e281f4ba48c3c23ad1
이지윤 <02leejiyun@naver.com>
2023-09-06 20:35:24 +0900

    Refactor: [parsing.h] 헤더에 함수 추가

--- parsing/parsing.h
+++ parsing/parsing.h
@@ -6,7 +6,7 @@
 /*   By: jiyunlee <jiyunlee@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/01 18:08:08 by jiyunlee          #+#    #+#             */
-/*   Updated: 2023/09/05 01:00:06 by jiyunlee         ###   ########.fr       */
+/*   Updated: 2023/09/06 20:29:02 by jiyunlee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@
 # include <readline/readline.h>
 # include <readline/history.h>
 
-void	env_init(t_env_info **env_info, char **envp);
+void	env_init(t_env_info **env, char **envp);
+void	cmd_init(t_cmd_info **cmd, char *str);
 
 #endif

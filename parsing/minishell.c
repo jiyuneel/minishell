6105fea90700bd63c4ad0c39eb1f48d7e2576e69
이지윤 <02leejiyun@naver.com>
2023-09-06 20:36:32 +0900

    Feat: [minishell.c] env 처리 수정

--- parsing/minishell.c
+++ parsing/minishell.c
@@ -6,7 +6,7 @@
 /*   By: jiyunlee <jiyunlee@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/30 19:51:59 by jiyunlee          #+#    #+#             */
-/*   Updated: 2023/09/06 20:11:33 by jiyunlee         ###   ########.fr       */
+/*   Updated: 2023/09/06 20:23:54 by jiyunlee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,21 @@
 
 int	main(int argc, char **argv, char **envp)
 {
-	// t_shell_info	shell_info;
-	t_env_info		*env_info;
+	t_shell_info	shell_info;
 	char			*str;
 
 	(void) argv;
 	if (argc != 1)
 		return (1);		// error
-	env_init(&env_info, envp);
-	for (t_env_info *tmp = env_info; tmp; tmp = tmp->next)
+	env_init(&shell_info.env, envp);
+	for (t_env_info *tmp = shell_info.env; tmp; tmp = tmp->next)
 		ft_printf("key: %s, value: %s\n", tmp->key, tmp->value);
 	while (1)
 	{
 		str = readline("jijishell$ ");
 		if (!str)		// ctrl+D
 			break ;
-		// cmd_init()
+		cmd_init(&shell_info.cmd_node, str);
 		// ft_printf("str = %s\n", str);
 		add_history(str);
 		free(str);

6e8851e7bf982da7da357a6e463f4c467245e4fb
Jiyun Lee <02leejiyun@naver.com>
2023-09-07 14:53:36 +0900

    Feat: [minishell.c] 입력을 받을 때마다 shell_info memset

--- parsing/minishell.c
+++ parsing/minishell.c
@@ -6,7 +6,7 @@
 /*   By: jiyunlee <jiyunlee@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/30 19:51:59 by jiyunlee          #+#    #+#             */
-/*   Updated: 2023/09/06 21:04:02 by jiyunlee         ###   ########.fr       */
+/*   Updated: 2023/09/07 14:50:02 by jiyunlee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,15 @@ int	main(int argc, char **argv, char **envp)
 	if (argc != 1)
 		return (1);		// error
 	env_init(&shell_info.env, envp);
-	for (t_env_info *tmp = shell_info.env; tmp; tmp = tmp->next)
-		ft_printf("key: %s, value: %s\n", tmp->key, tmp->value);
+	/* env 출력 */
+	// for (t_env_info *tmp = shell_info.env; tmp; tmp = tmp->next)
+	// 	ft_printf("key: %s, value: %s\n", tmp->key, tmp->value);
 	while (1)
 	{
 		str = readline("jijishell$ ");
 		if (!str)		// ctrl+D
 			break ;
+		ft_memset(&shell_info, 0, sizeof(t_shell_info));
 		shell_init(&shell_info, str);
 		// ft_printf("str = %s\n", str);
 		add_history(str);

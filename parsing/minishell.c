2193794e939d646a6c229688dadf33d54029f229
이지윤 <02leejiyun@naver.com>
2023-09-11 22:02:48 +0900

    Feat: [minishell.c] 마지막에 env_info free 해주기

--- parsing/minishell.c
+++ parsing/minishell.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jihykim2 <jihykim2@student.42seoul.kr>     +#+  +:+       +#+        */
+/*   By: jiyunlee <jiyunlee@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/30 19:51:59 by jiyunlee          #+#    #+#             */
-/*   Updated: 2023/09/07 16:29:27 by jihykim2         ###   ########.fr       */
+/*   Updated: 2023/09/11 00:40:58 by jiyunlee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,6 @@ int	main(int argc, char **argv, char **envp)
 	if (argc != 1)
 		return (1);		// error
 	env_init(&shell_info.env, envp);
-	/* env 출력 */
-	// for (t_env_info *tmp = shell_info.env; tmp; tmp = tmp->next)
-	// 	ft_printf("key: %s, value: %s\n", tmp->key, tmp->value);
 	while (1)
 	{
 		str = readline("jijishell$ ");
@@ -35,5 +32,6 @@ int	main(int argc, char **argv, char **envp)
 		add_history(str);
 		free(str);
 	}
+	// free_env_info(shell_info.env);
 	return (0);
 }

66a4db364cc30b25941164c79cbde380204294e1
이지윤 <02leejiyun@naver.com>
2023-09-13 16:40:03 +0900

    Feat: [minishell.c] print_shell_info 함수 구현

--- parsing/minishell.c
+++ parsing/minishell.c
@@ -6,11 +6,13 @@
 /*   By: jiyunlee <jiyunlee@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/30 19:51:59 by jiyunlee          #+#    #+#             */
-/*   Updated: 2023/09/11 00:40:58 by jiyunlee         ###   ########.fr       */
+/*   Updated: 2023/09/13 16:38:27 by jiyunlee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parsing.h"
+
+void	print_shell_info(t_shell_info *shell_info);
 
 int	main(int argc, char **argv, char **envp)
 {
@@ -28,10 +30,29 @@ int	main(int argc, char **argv, char **envp)
 			break ;
 		shell_info.chunk_cnt = 0;
 		shell_init(&shell_info, str);
+		// print_shell_info(&shell_info);
 		execute(&shell_info);
 		add_history(str);
 		free(str);
 	}
 	// free_env_info(shell_info.env);
 	return (0);
+}
+
+/* shell_info 출력 */
+void	print_shell_info(t_shell_info *shell_info)
+{
+	ft_printf("chunk_cnt: %d\n\n", shell_info->chunk_cnt);
+	for (t_cmd_info *tmp = shell_info->cmd; tmp; tmp = tmp->next)
+	{
+		ft_printf("[----- cmd -----]\n");
+		ft_printf("cmd_cnt: %d\n", tmp->cmd_cnt);
+		for (int i = 0; tmp->cmd_args[i]; i++)
+			ft_printf("%s\n", tmp->cmd_args[i]);
+		ft_printf("[---- redir ----]\n");
+		for (t_redir *r = tmp->redir; r; r = r->next) {
+			ft_printf("%d %s\n", r->type, r->filename);
+		}
+		ft_printf("\n-----------------\n\n");
+	}
 }

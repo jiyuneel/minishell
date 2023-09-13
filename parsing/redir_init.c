30f3a7060be8f23d770c4da1d152028108a682e1
이지윤 <02leejiyun@naver.com>
2023-09-13 20:14:23 +0900

    Refactor: [redir_init.c] redir_init 함수 정리

--- parsing/redir_init.c
+++ parsing/redir_init.c
@@ -6,7 +6,7 @@
 /*   By: jiyunlee <jiyunlee@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/12 01:00:07 by jiyunlee          #+#    #+#             */
-/*   Updated: 2023/09/13 16:31:51 by jiyunlee         ###   ########.fr       */
+/*   Updated: 2023/09/13 20:07:06 by jiyunlee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,17 +40,6 @@ void	redir_add_back(t_redir **node, t_redir *new)
 	}
 }
 
-// void	delete_cmd(t_cmd_info *cmd_info, int idx)
-// {
-// 	free(cmd_info->cmd_args[idx]);
-// 	while (cmd_info->cmd_args[idx])
-// 	{
-// 		cmd_info->cmd_args[idx] = cmd_info->cmd_args[idx + 1];
-// 		idx++;
-// 	}
-// 	cmd_info->cmd_cnt--;
-// }
-
 void	delete_cmd(t_cmd_info *cmd_info, char **cmd)
 {
 	free(*cmd);
@@ -62,6 +51,35 @@ void	delete_cmd(t_cmd_info *cmd_info, char **cmd)
 	cmd_info->cmd_cnt--;
 }
 
+char	*set_redir_filename(t_cmd_info *cmd_info, char **args, t_token_type type)
+{
+	char	*filename;
+
+	filename = NULL;
+	if ((type == LEFT_1 || type == RIGHT_1) && !(*args)[1])
+	{
+		filename = ft_strdup(*(args + 1));
+		delete_cmd(cmd_info, args + 1);
+	}
+	else if ((type == LEFT_1 || type == RIGHT_1) && (*args)[1])
+	{
+		filename = malloc(sizeof(char) * ft_strlen(*args));
+		ft_strlcpy(filename, &(*args)[1], ft_strlen(*args));
+	}
+	else if ((type == LEFT_2 || type == RIGHT_2) && !(*args)[2])
+	{
+		filename = ft_strdup(*(args + 1));
+		delete_cmd(cmd_info, args + 1);
+	}
+	else if ((type == LEFT_2 || type == RIGHT_2) && (*args)[2])
+	{
+		filename = malloc(sizeof(char) * ft_strlen(*args) - 1);
+		ft_strlcpy(filename, &(*args)[2], ft_strlen(*args) - 1);
+	}
+	delete_cmd(cmd_info, args);
+	return (filename);
+}
+
 void	redir_init(t_cmd_info *cmd_info)
 {
 	char			**tmp_args;
@@ -71,117 +89,20 @@ void	redir_init(t_cmd_info *cmd_info)
 	tmp_args = cmd_info->cmd_args;
 	while (*tmp_args)
 	{
-		if ((*tmp_args)[0] == '<' && (*tmp_args)[1] != '<')
+		if ((*tmp_args)[0] == '<' || (*tmp_args)[0] == '>')
 		{
-			type = LEFT_1;
-			if (!(*tmp_args)[1])
-			{
-				filename = ft_strdup(*(tmp_args + 1));
-				delete_cmd(cmd_info, tmp_args + 1);
-				delete_cmd(cmd_info, tmp_args);
-			}
-			else
-			{
-				filename = malloc(sizeof(char) * ft_strlen(*tmp_args));
-				ft_strlcpy(filename, &(*tmp_args)[1], ft_strlen(*tmp_args));
-				delete_cmd(cmd_info, tmp_args);
-			}
-			redir_add_back(&cmd_info->redir, redir_new_node(type, filename));
-		}
-		else if ((*tmp_args)[0] == '<' && (*tmp_args)[1] == '<')
-		{
-			type = LEFT_2;
-			if (!(*tmp_args)[2])
-			{
-				filename = ft_strdup(*(tmp_args + 1));
-				delete_cmd(cmd_info, tmp_args + 1);
-				delete_cmd(cmd_info, tmp_args);
-			}
-			else
-			{
-				filename = malloc(sizeof(char) * ft_strlen(*tmp_args) - 1);
-				ft_strlcpy(filename, &(*tmp_args)[2], ft_strlen(*tmp_args) - 1);
-				delete_cmd(cmd_info, tmp_args);
-			}
-			redir_add_back(&cmd_info->redir, redir_new_node(type, filename));
-		}
-		else if ((*tmp_args)[0] == '>' && (*tmp_args)[1] != '>')
-		{
-			type = RIGHT_1;
-			if (!(*tmp_args)[1])
-			{
-				filename = ft_strdup(*(tmp_args + 1));
-				delete_cmd(cmd_info, tmp_args + 1);
-				delete_cmd(cmd_info, tmp_args);
-			}
-			else
-			{
-				filename = malloc(sizeof(char) * ft_strlen(*tmp_args));
-				ft_strlcpy(filename, &(*tmp_args)[1], ft_strlen(*tmp_args));
-				delete_cmd(cmd_info, tmp_args);
-			}
-			redir_add_back(&cmd_info->redir, redir_new_node(type, filename));
-		}
-		else if ((*tmp_args)[0] == '>' && (*tmp_args)[1] == '>')
-		{
-			type = RIGHT_2;
-			if (!(*tmp_args)[2])
-			{
-				filename = ft_strdup(*(tmp_args + 1));
-				delete_cmd(cmd_info, tmp_args + 1);
-				delete_cmd(cmd_info, tmp_args);
-			}
-			else
-			{
-				filename = malloc(sizeof(char) * ft_strlen(*tmp_args) - 1);
-				ft_strlcpy(filename, &(*tmp_args)[2], ft_strlen(*tmp_args) - 1);
-				delete_cmd(cmd_info, tmp_args);
-			}
+			if ((*tmp_args)[0] == '<' && (*tmp_args)[1] != '<')
+				type = LEFT_1;
+			else if ((*tmp_args)[0] == '<' && (*tmp_args)[1] == '<')
+				type = LEFT_2;
+			else if ((*tmp_args)[0] == '>' && (*tmp_args)[1] != '>')
+				type = RIGHT_1;
+			else if ((*tmp_args)[0] == '>' && (*tmp_args)[1] == '>')
+				type = RIGHT_2;
+			filename = set_redir_filename(cmd_info, tmp_args, type);
 			redir_add_back(&cmd_info->redir, redir_new_node(type, filename));
 		}
 		else
 			tmp_args++;
 	}
 }
-
-// void	redir_init(t_cmd_info *cmd_info)
-// {
-// 	int				i;
-// 	t_token_type	type;
-// 	char			*filename;
-
-// 	i = 0;
-// 	while (cmd_info->cmd_args[i])
-// 	{
-// 		if (cmd_info->cmd_args[i][0] == '<' && cmd_info->cmd_args[i][1] != '<')
-// 		{
-// 			type = LEFT_1;
-// 			if (!cmd_info->cmd_args[i][1])
-// 			{
-// 				filename = ft_strdup(cmd_info->cmd_args[i + 1]);
-// 				delete_cmd(cmd_info, i + 1);
-// 				delete_cmd(cmd_info, i);
-// 			}
-// 			else
-// 			{
-// 				filename = malloc(sizeof(char) * ft_strlen(cmd_info->cmd_args[i]));
-// 				ft_strlcpy(filename, &cmd_info->cmd_args[i][1], ft_strlen(cmd_info->cmd_args[i]));
-// 			}
-// 			redir_add_back(&cmd_info->redir, redir_new_node(type, filename));
-// 		}
-// 		else if (cmd_info->cmd_args[i][0] == '<' && cmd_info->cmd_args[i][1] == '<')
-// 		{
-// 			type = LEFT_2;
-// 		}
-// 		else if (cmd_info->cmd_args[i][0] == '>' && cmd_info->cmd_args[i][1] != '>')
-// 		{
-// 			type = RIGHT_1;
-// 		}
-// 		else if (cmd_info->cmd_args[i][0] == '>' && cmd_info->cmd_args[i][1] == '>')
-// 		{
-// 			type = RIGHT_2;
-// 		}
-// 		else
-// 			i++;
-// 	}
-// }

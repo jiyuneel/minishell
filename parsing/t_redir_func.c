325061493fe0f913ebb71410ecf5b89b35672a20
이지윤 <02leejiyun@naver.com>
2023-09-17 21:55:09 +0900

    Add: [t_redir_func.c] redir 관련 함수 파일 추가

---
+++ parsing/t_redir_func.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   t_redir_func.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jiyunlee <jiyunlee@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/17 21:18:25 by jiyunlee          #+#    #+#             */
+/*   Updated: 2023/09/17 21:18:46 by jiyunlee         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "parsing.h"
+
+t_redir	*redir_new_node(t_token_type type, char *filename)
+{
+	t_redir	*node;
+
+	node = malloc(sizeof(t_redir));
+	if (!node)
+		return (NULL);
+	node->type = type;
+    node->filename = filename;
+	node->next = NULL;
+	return (node);
+}
+
+void	redir_add_back(t_redir **node, t_redir *new)
+{
+	t_redir	*tmp;
+
+	if (!(*node))
+		*node = new;
+	else
+	{
+		tmp = *node;
+		while (tmp->next)
+			tmp = tmp->next;
+		tmp->next = new;
+	}
+}

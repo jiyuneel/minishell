b4f169a1d726d9e52df7fd7ffad0222bc60bccb2
Jihyun Kim <ksk208105@gmail.com>
2023-09-22 19:54:18 +0900

    Feat: [multi_process.c] wait에서 exit_code 및 시그널 종료에 대한 처리

--- execute/multi_process.c
+++ execute/multi_process.c
@@ -6,11 +6,11 @@
 /*   By: jihykim2 <jihykim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/05 13:01:06 by jihykim2          #+#    #+#             */
-/*   Updated: 2023/09/22 02:29:34 by jihykim2         ###   ########.fr       */
+/*   Updated: 2023/09/22 16:39:55 by jihykim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "../includes/minishell.h"
+#include "../includes/minishell.h"
 
 static void	_parent_process(t_exec_info *exec);
 static void	_child_process(t_exec_info *exec, t_cmd_info *cmd, int end);
@@ -65,9 +65,36 @@ static void	_wait_child(pid_t last_pid, int chunk_cnt)
 	pid_t	child;
 	int		status;
 	int		idx;
+	int		print_flag;
 
 	idx = 0;
-	// printf("last: %d\n", last_pid);
+	print_flag = 0;
+	while (idx < chunk_cnt)
+	{
+		child = waitpid(-1, &status, 0);
+		if (WIFSIGNALED(status) == TRUE && ++print_flag)
+		{
+			if (WTERMSIG(status) == SIGINT && print_flag == TRUE)
+				ft_putstr_fd("^C\n", STDERR_FILENO);
+			else if (WTERMSIG(status) == SIGQUIT && print_flag == TRUE)
+				ft_putstr_fd("^\\Quit: 3\n", STDERR_FILENO);
+			if (child == last_pid)
+				g_exit_code = 128 + WTERMSIG(status);
+		}
+		else if (child == last_pid)
+			g_exit_code = WEXITSTATUS(status);
+		idx++;
+	}
+}
+/*
+static void	_wait_child(pid_t last_pid, int chunk_cnt)
+{
+	pid_t	child;
+	int		status;
+	int		idx;
+
+	idx = 0;
+	printf("last: %d\n", last_pid);
 	while (idx < chunk_cnt)
 	{
 		child = waitpid(-1, &status, 0);
@@ -78,10 +105,14 @@ static void	_wait_child(pid_t last_pid, int chunk_cnt)
 				ft_putstr_fd("^C\n", STDERR_FILENO);
 			else if (WTERMSIG(status) == SIGQUIT)
 				ft_putstr_fd("^\\Quit: 3\n", STDERR_FILENO);
-			g_exit_code = 128 + WTERMSIG(status);
+			if (child == last_pid)
+				g_exit_code = 128 + WTERMSIG(status);
 		}
-		if (child == last_pid)
+		else if (child == last_pid)
 			g_exit_code = WEXITSTATUS(status);
+		printf("child: %d, exitcode: %d\n", child, status);
 		idx++;
 	}
+	printf("[exitcode: %d]\n", g_exit_code);
 }
+*/

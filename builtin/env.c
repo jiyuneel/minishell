eac6fc279c08fd0fb043aaefbb6b2ed396e9c8a4
Jiyun Lee <02leejiyun@naver.com>
2023-09-25 17:21:21 +0900

    Fix: [env.c] value가 NULL인 경우 예외처리

--- builtin/env.c
+++ builtin/env.c
@@ -6,20 +6,22 @@
 /*   By: jiyunlee <jiyunlee@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 10:11:12 by jiyunlee          #+#    #+#             */
-/*   Updated: 2023/09/21 19:29:15 by jiyunlee         ###   ########.fr       */
+/*   Updated: 2023/09/25 16:03:42 by jiyunlee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-void	env(t_exec_info *exec)
+int	env(t_exec_info *exec)
 {
 	t_env_info	*tmp;
 
 	tmp = exec->env;
 	while (tmp)
 	{
-		printf("%s=%s\n", tmp->key, tmp->value);
+		if (tmp->value)
+			printf("%s=%s\n", tmp->key, tmp->value);
 		tmp = tmp->next;
 	}
+	return (EXIT_SUCCESS);
 }

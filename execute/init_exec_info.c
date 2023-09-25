6ba3866451e239b160435b4a5c702268abcebf49
Jihyun Kim <ksk208105@gmail.com>
2023-09-25 17:18:40 +0900

    Fix: [struct] t_exec_info에서 env에 대한 주소 값을 넘겨주는 것으로 변경

--- execute/init_exec_info.c
+++ execute/init_exec_info.c
@@ -6,7 +6,7 @@
 /*   By: jihykim2 <jihykim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/02 20:40:35 by jihykim2          #+#    #+#             */
-/*   Updated: 2023/09/25 16:11:13 by jihykim2         ###   ########.fr       */
+/*   Updated: 2023/09/25 16:33:32 by jihykim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ t_exec_info	*init_exec_info(t_shell_info *parse)
 	if (exec == NULL)
 		return (NULL);		// 어떻게 처리할 지 고민(signal ?)
 	ft_memset(exec, 0, sizeof(t_exec_info));
-	exec->env = parse->env;
+	exec->env = &(parse->env);
 	_get_path_args(exec);
 	_get_envp(exec);
 	return (exec);
@@ -34,7 +34,7 @@ static void	_get_path_args(t_exec_info *exec)
 {
 	t_env_info	*node;
 
-	node = exec->env;
+	node = *exec->env;
 	while (node)
 	{
 		if (ft_strcmp(node->key, "PATH") == 0)		// strncmp로 할건지 고민
@@ -57,7 +57,7 @@ static void	_get_envp(t_exec_info *exec)
 	char		*tmp;
 	int			idx;
 
-	node = exec->env;
+	node = *exec->env;
 	if (node == NULL)
 		return ;
 	exec->envp = ft_calloc(_get_envp_len(node) + 1, sizeof(char *));

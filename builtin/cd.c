b4f0641df3268628009b5436f5e7f02d8cc11ff9
Jihyun Kim <ksk208105@gmail.com>
2023-09-25 17:11:27 +0900

    Add: [is_builtin.c] check is builtin functions

--- builtin/cd.c
+++ builtin/cd.c
@@ -1,26 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   env.c                                              :+:      :+:    :+:   */
+/*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jihykim2 <jihykim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/20 10:11:12 by jiyunlee          #+#    #+#             */
-/*   Updated: 2023/09/25 17:00:46 by jihykim2         ###   ########.fr       */
+/*   Created: 2023/09/25 16:52:40 by jihykim2          #+#    #+#             */
+/*   Updated: 2023/09/25 16:53:22 by jihykim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-int	env(t_exec_info *exec)
+int	cd(t_exec_info *exec)
 {
-	t_env_info	*tmp;
-
-	tmp = *exec->env;
-	while (tmp)
-	{
-		printf("%s=%s\n", tmp->key, tmp->value);
-		tmp = tmp->next;
-	}
+	(void)exec;
 	return (EXIT_SUCCESS);
 }
--- builtin/exit_with_args.c
+++ builtin/exit_with_args.c
@@ -1,26 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   env.c                                              :+:      :+:    :+:   */
+/*   exit_with_args.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jihykim2 <jihykim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/20 10:11:12 by jiyunlee          #+#    #+#             */
-/*   Updated: 2023/09/25 17:00:46 by jihykim2         ###   ########.fr       */
+/*   Created: 2023/09/25 16:52:40 by jihykim2          #+#    #+#             */
+/*   Updated: 2023/09/25 16:56:09 by jihykim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-int	env(t_exec_info *exec)
+int	exit_with_args(t_exec_info *exec)
 {
-	t_env_info	*tmp;
-
-	tmp = *exec->env;
-	while (tmp)
-	{
-		printf("%s=%s\n", tmp->key, tmp->value);
-		tmp = tmp->next;
-	}
-	return (EXIT_SUCCESS);
+	(void)exec;
+	return (g_exit_code);
 }

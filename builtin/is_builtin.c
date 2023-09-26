9243ee67ceb1360ec673d763f9d6d07100129895
Jihyun Kim <ksk208105@gmail.com>
2023-09-27 05:51:44 +0900

    Fix: [is_builtin.c] 빈 문자열이 들어오는 경우 DEADLYSIGNAL 발생 -> 해결

--- builtin/is_builtin.c
+++ builtin/is_builtin.c
@@ -6,7 +6,7 @@
 /*   By: jihykim2 <jihykim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 11:28:46 by jihykim2          #+#    #+#             */
-/*   Updated: 2023/09/26 23:20:19 by jihykim2         ###   ########.fr       */
+/*   Updated: 2023/09/27 05:29:20 by jihykim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,22 @@ int	is_builtin(t_exec_info *exec, int *exit_code, int child)
 	int	flag;
 
 	flag = FALSE;
-	// if (ft_strcmp(exec->cmd_args[0], "echo") == 0 && ++flag)
-	// 	*exit_code = echo(exec);
-	if (ft_strcmp(exec->cmd_args[0], "cd") == 0 && ++flag)		// parent
+	if (exec->cmd_args[0] == NULL)
+		return (FALSE);
+	if (ft_strcmp(exec->cmd_args[0], "echo") == 0 && ++flag)
+		*exit_code = echo(exec);
+	else if (ft_strcmp(exec->cmd_args[0], "cd") == 0 && ++flag)
 		*exit_code = cd(exec);
-	else if (ft_strcmp(exec->cmd_args[0], "pwd") == 0 && ++flag)	// parent
+	else if (ft_strcmp(exec->cmd_args[0], "pwd") == 0 && ++flag)
 		*exit_code = pwd(exec);
-	else if (ft_strcmp(exec->cmd_args[0], "export") == 0 && ++flag)	// parent
+	else if (ft_strcmp(exec->cmd_args[0], "export") == 0 && ++flag)
 		*exit_code = export(exec);
-	else if (ft_strcmp(exec->cmd_args[0], "unset") == 0 && ++flag)	// parent
+	else if (ft_strcmp(exec->cmd_args[0], "unset") == 0 && ++flag)
 		*exit_code = unset(exec);
 	else if (ft_strcmp(exec->cmd_args[0], "env") == 0 && ++flag)
 		*exit_code = env(exec);
-	else if (ft_strcmp(exec->cmd_args[0], "exit") == 0 && ++flag)	// parent
-		*exit_code = exit_with_args(exec, child);		// exit(g_exit_code)인지 다시 확인하기...
+	else if (ft_strcmp(exec->cmd_args[0], "exit") == 0 && ++flag)
+		*exit_code = exit_with_args(exec, child);
 	if (flag == FALSE)
 		return (FALSE);
 	return (TRUE);

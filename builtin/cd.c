76520b29837c5adb00ba75075c64c69924460a33
Jihyun Kim <ksk208105@gmail.com>
2023-09-27 12:52:59 +0900

    Fix: [cd.c] '.'과 '..'등 상대 경로가 들어간 경우 /Users/jihyun/minishell 값 수정

--- builtin/cd.c
+++ builtin/cd.c
@@ -6,19 +6,19 @@
 /*   By: jihykim2 <jihykim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 16:52:40 by jihykim2          #+#    #+#             */
-/*   Updated: 2023/09/27 05:39:14 by jihykim2         ###   ########.fr       */
+/*   Updated: 2023/09/27 12:28:07 by jihykim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
 static char	*_get_home_path(t_env_info **env);
-static void	_change_pwd(t_env_info **env, char *curr, char *prev);
+static void	_change_pwd(t_env_info **env, char *prev_path);
 
 int	cd(t_exec_info *exec)
 {
 	char	*path;
-	char	*origin;
+	char	*prev_path;
 
 	if (exec->cmd_args[1] == NULL || ft_strcmp(exec->cmd_args[1], "~") == 0)
 		path = _get_home_path(exec->env);
@@ -29,17 +29,17 @@ int	cd(t_exec_info *exec)
 		error_message("cd", NULL, "HOME not set");
 		return (EXIT_FAILURE);
 	}
-	origin = getcwd(NULL, 0);
+	prev_path = getcwd(NULL, 0);
 	if (chdir(path) == EXIT_SUCCESS)
-		_change_pwd(exec->env, path, origin);
+		_change_pwd(exec->env, prev_path);
 	else
 	{
-		free (origin);
+		free (prev_path);
 		ft_putstr_fd("jijishell: ", STDERR_FILENO);
 		perror("cd");
 		return (EXIT_FAILURE);
 	}
-	free (origin);
+	free (prev_path);
 	return (EXIT_SUCCESS);
 }
 
@@ -57,7 +57,7 @@ static char	*_get_home_path(t_env_info **env)
 	return (NULL);
 }
 
-static void	_change_pwd(t_env_info **env, char *curr, char *prev)
+static void	_change_pwd(t_env_info **env, char *prev_path)
 {
 	t_env_info	*node;
 
@@ -68,16 +68,13 @@ static void	_change_pwd(t_env_info **env, char *curr, char *prev)
 		{
 			if (node->value != NULL)
 				free (node->value);
-			if (ft_strcmp(curr, ".") == 0)
-				node->value = getcwd(NULL, 0);
-			else
-				node->value = ft_strdup(curr);
+			node->value = getcwd(NULL, 0);
 		}
 		else if (strcmp(node->key, "OLDPWD") == 0)
 		{
 			if (node->value != NULL)
 				free (node->value);
-			node->value = ft_strdup(prev);
+			node->value = ft_strdup(prev_path);
 		}
 		node = node->next;
 	}
